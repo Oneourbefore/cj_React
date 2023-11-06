7c9cde89b4478a5bdb1e573a33f230518714a7b9
KyoungHyun <102707496+namsta9ramm@users.noreply.github.com>
2023-11-06 21:21:33 +0900

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Growth from './gworth'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      
+    <div class='e408_335'>
+  <div class="e408_336"></div><span  class="e408_337">GrowHub</span>
+  <div class='e408_338'>
+    <div class="e408_339"></div><span  class="e408_340">사용자 ID</span><span  class="e408_341">사용자 이름</span>
+    <div class='e408_342'>
+      <div class="ei408_342_2474_641"></div>
+      <div class="ei408_342_2474_642"></div>
+      <div class="ei408_342_2474_643"></div>
     </div>
-  );
-}
+  </div>
+  <div class="e408_343"></div><span  class="e408_344">ID</span><span  class="e408_345">사용자 닉네임</span>
+  <div class="e408_346"></div>
+  <div class='e408_347'><span  class="e408_348">성장도</span><span  class="e408_349">인증</span><span  class="e408_350"> <a>쇼핑</a> </span></div>
+  <div class="e408_351"></div>
+  <div class="e408_352"></div>
+  <div class='e408_353'>
+    <div class='e408_354'>
+      <div class='e408_355'>
+        <div class="ei408_355_2474_753"></div>
+      </div><span  class="e408_356">검색</span><span  class="e408_357">메시지</span><span  class="e408_358">알림</span><span  class="e408_359">내 그룹</span><span  class="e408_360">글쓰기</span><span  class="e408_361">병원</span><span  class="e408_362">리워드</span><span  class="e408_363">홈 </span>
+      <div class='e408_364'>
+        <div class="ei408_364_2474_659"></div>
+        <div class="ei408_364_2474_660"></div>
+      </div>
+      <div class='e408_365'>
+        <div class="ei408_365_2474_538"></div>
+      </div>
+      <div class='e408_366'>
+        <div class="ei408_366_2474_892"></div>
+        <div class="ei408_366_2474_893"></div>
+        <div class="ei408_366_2474_894"></div>
+        <div class="ei408_366_2474_895"></div>
+      </div>
+      <div class='e408_367'>
+        <div class="ei408_367_2474_594"></div>
+      </div>
+      <div class='e408_368'>
+        <div class="ei408_368_2474_530"></div>
+      </div>
+      <div class='e408_369'>
+        <div class="ei408_369_2474_418"></div>
+        <div class="ei408_369_2474_419"></div>
+      </div>
+    </div>
+    <div class='e408_370'>
+      <div class="ei408_370_2474_625"></div>
+    </div>
+  </div>
+  <div class='e408_371'>
+    <div class="ei408_371_2474_627"></div>
+    <div class="ei408_371_2474_628"></div>
+    <div class="ei408_371_2474_629"></div>
+    <div class="ei408_371_2474_630"></div>
+  </div><span  class="e408_372">0000캐시</span>
+  <div class='e408_415'>
+    <div class="e408_373"></div>
+    <div class='e408_374'>
+      <div class='e408_375'>
+        <div class="ei408_375_2474_686"></div>
+        <div class="ei408_375_2474_687"></div>
+        <div class="ei408_375_2474_688"></div>
+      </div><span  class="e408_376">사진 업로드하기</span>
+    </div>
+  </div>
+</div>
+  )
+
+  }
 
 export default App;
+
+
---
+++ src/gworth.js
@@ -0,0 +1,101 @@
+import logo from './logo.svg';
+import './App.css';
+import { useNavigate } from 'react-router-dom';
+function Growth() {
+
+
+    return(
+    <div class='e157_511'>
+        <div class="e157_515"></div><span  class="e157_516">GrowHub</span>
+        <div class='e157_517'>
+    <div class="e157_518"></div><span  class="e157_519">사용자 ID</span><span  class="e157_520">사용자 이름</span>
+    <div class='e157_521'>
+        <div class="ei157_521_2474_641"></div>
+        <div class="ei157_521_2474_642"></div>
+        <div class="ei157_521_2474_643"></div>
+    </div>
+    </div>
+    <div class="e157_522"></div><span  class="e157_523">ID</span><span  class="e157_524">사용자 닉네임</span>
+    <div class="e157_525"></div>
+    <div class='e157_526'><span  class="e157_527">성장도</span><span  class="e157_528">인증</span><span  class="e157_529">쇼핑</span></div>
+    <div class="e157_530"></div>
+    <div class="e157_531"></div>
+    <div class='e157_533'>
+        <div class='e157_534'>
+        <div class='e157_535'>
+            <div class="ei157_535_2474_753"></div>
+        </div><span  class="e157_536">검색</span><span  class="e157_537">메시지</span><span  class="e157_538">알림</span><span  class="e157_539">내 그룹</span><span  class="e157_540">글쓰기</span><span  class="e157_541">병원</span><span  class="e157_542">리워드</span><span  class="e157_543">홈 </span>
+        <div class='e157_544'>
+            <div class="ei157_544_2474_659"></div>
+            <div class="ei157_544_2474_660"></div>
+        </div>
+        <div class='e157_545'>
+            <div class="ei157_545_2474_538"></div>
+        </div>
+        <div class='e157_546'>
+            <div class="ei157_546_2474_892"></div>
+            <div class="ei157_546_2474_893"></div>
+            <div class="ei157_546_2474_894"></div>
+            <div class="ei157_546_2474_895"></div>
+        </div>
+        <div class='e157_547'>
+            <div class="ei157_547_2474_594"></div>
+        </div>
+        <div class='e157_548'>
+            <div class="ei157_548_2474_530"></div>
+        </div>
+        <div class='e157_549'>
+            <div class="ei157_549_2474_418"></div>
+            <div class="ei157_549_2474_419"></div>
+        </div>
+        </div>
+        <div class='e157_550'>
+        <div class="ei157_550_2474_625"></div>
+        </div>
+    </div>
+    <div class='e157_551'>
+        <div class="ei157_551_2474_627"></div>
+        <div class="ei157_551_2474_628"></div>
+        <div class="ei157_551_2474_629"></div>
+        <div class="ei157_551_2474_630"></div>
+    </div><span  class="e157_552">0000 캐시</span>
+    <div class='e157_648'>
+        <div class="e157_649"></div><span  class="e157_650">식물 이름
+
+    00일 째
+
+            
+    </span>
+    </div>
+    <div class='e158_683'>
+        <div class="e158_684"></div><span  class="e158_685">식물 이름 00일 째
+
+    </span>
+    </div>
+    <div class='e157_651'>
+        <div class="ei157_651_2474_481"></div>
+    </div>
+    <div class='e158_686'>
+        <div class="ei158_686_2474_481"></div>
+    </div>
+    <div class="e157_646"></div>
+    <div class="e158_687"></div>
+    <div class="e157_661"></div>
+    <div class="e158_688"></div><span  class="e157_647"> 정식기</span><span  class="e158_689"> 정식기</span><span  class="e157_662">상추</span><span  class="e158_690">상추</span>
+    <div class='e157_653'>
+        <div class="ei157_653_2474_799"></div>
+        <div class="ei157_653_2474_800"></div>
+        <div class="ei157_653_2474_801"></div>
+    </div>
+    <div class='e158_691'>
+        <div class="ei158_691_2474_799"></div>
+        <div class="ei158_691_2474_800"></div>
+        <div class="ei158_691_2474_801"></div>
+    </div>
+    <div class="e158_678"></div><span  class="e158_679">정상적으로 잘 크고 있습니다!곧 생육기로 접어들 거예요 .</span>
+    <div class="e158_680"></div>
+    <div class="e158_682"></div>
+</div>
+    )
+}
+export default Growth;
